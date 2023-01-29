2191f6c1299e714a9fcd601ce45747f396a2c47c
Eshetu Lucas <tekalukas@gmail.com>
2023-01-29 16:20:12 +0300

    Updates widgets and other related codes

--- lib/ui/views/home/widgets/image_widget.dart
+++ lib/ui/views/home/widgets/image_widget.dart
@@ -5,14 +5,16 @@ class ImageWidget extends StatelessWidget {
   const ImageWidget({
     required this.fit,
     Key? key,
+    this.imagePath = 'assets/images/placeholder.png',
   }) : super(key: key);
 
   final BoxFit fit;
+  final String imagePath;
 
   @override
   Widget build(BuildContext context) {
     return ImageBuilder(
-      imagePath: 'assets/images/placeholder.png',
+      imagePath: imagePath,
       fit: fit,
     );
   }
